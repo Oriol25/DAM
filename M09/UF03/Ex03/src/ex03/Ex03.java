4845e1303acaf2c56e02ab5bdcceda47aa2ff5bb
Oriol25 <71832303+Oriol25@users.noreply.github.com>
2021-03-22 00:09:50 +0100

    Add files via upload

--- M09/UF03/Ex03/src/ex03/Ex03.java
+++ M09/UF03/Ex03/src/ex03/Ex03.java
@@ -1,11 +1,24 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ex03;
 
-import java.net.*;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
 
-
+/**
+ *
+ * @author poved
+ */
 public class Ex03 {
 
     @SuppressWarnings("rawtypes")
@@ -53,7 +66,9 @@ public class Ex03 {
             BufferedReader pagina = new BufferedReader(new InputStreamReader(url.openStream()));
 			
             while ((cadena = pagina.readLine()) != null) {		
-                System.out.println(cadena);
+                if(cadena.contains(patro.subSequence(0, patro.length()-1))) {
+                   System.out.println(cadena);
+                }
             }
 			
         } catch (MalformedURLException e) { 
