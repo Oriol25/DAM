807d2a38a27d3a5b90760c6459512e6cae6213e6
Oriol25 <71832303+Oriol25@users.noreply.github.com>
2021-04-06 20:30:09 +0200

    Add files via upload

--- M09/UF03/Ex08/src/ex08/Ex08.java
+++ M09/UF03/Ex08/src/ex08/Ex08.java
@@ -10,97 +10,133 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 
 public class Ex08 {
 
-	private final String USER_AGENT = "Mozilla/5.0";
+    private final String USER_AGENT = "Mozilla/5.0";
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		HttpURLConnectionExample http = new HttpURLConnectionExample();
-
-		System.out.println("Testing 1 - Send Http GET request");
-		http.sendGet();
+        Ex08 http = new Ex08();
+                
+        Scanner teclat = new Scanner(System.in);
+                
+        int opcio = 0;
+                
+        do {
+            System.out.println("Testing 1 - Send Http GET request");
+            System.out.println("Testing 2 - Send Http POST request");
+            System.out.print("Opcio: ");
+            opcio = teclat.nextInt();
+            if (opcio == 1) {
+                http.sendGet();
+                System.out.println("");
+            } else if (opcio == 2) {
+                http.sendPost();
+                System.out.println("");
+            }
+                   
+        } while (opcio == 1 || opcio == 2);
 		
-		System.out.println("\nTesting 2 - Send Http POST request");
-		http.sendPost();
+        
 
-	}
+    }
 
-	// HTTP GET request
-	private void sendGet() throws Exception {
+    // HTTP GET request
+    private void sendGet() throws Exception {
 
-		String url = "http://www.insbaixcamp.org/";
+        //FLUX PER A ENTRADA ESTÀNDARD
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String url = "";
+        
+        System.out.println("\nURL? (https://www.google.com/)");
+        url = br.readLine();     
+        
+	try {
+            
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setRequestMethod("GET");
+
+            //add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+        
+        } catch (Exception e) {
+            System.out.println("Error con el URL " + url);
+        }
+
+    }
+	
+    // HTTP POST request
+    private void sendPost() throws Exception {
+
+	String url;
+        
+        //FLUX PER A ENTRADA ESTÀNDARD
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("\nURL (http://www.insbaixcamp.cat/moodle/my)");
+        url = br.readLine();
+        
+	URL obj = new URL(url);
+	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+	//add reuqest header
+	con.setRequestMethod("POST");
+	con.setRequestProperty("User-Agent", USER_AGENT);
+	con.setRequestProperty("Accept-Language", "ca-es");
 		
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	//Query string
+	String urlParameters = "categoryid=7";
+		
+	// Send post request
+	con.setDoOutput(true);
+	DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+	wr.writeBytes(urlParameters);
+	wr.flush();
+	wr.close();
 
-		// optional default is GET
-		con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+	System.out.println("\nSending 'POST' request to URL : " + url);
+	System.out.println("Post parameters : " + urlParameters);
+	System.out.println("Response Code : " + responseCode);
 
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
+	BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+	String inputLine;
+	StringBuffer response = new StringBuffer();
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		System.out.println(response.toString());
-
+	while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
 	}
 	
-	// HTTP POST request
-	private void sendPost() throws Exception {
-
-		String url = "http://www.insbaixcamp.cat/moodle/";
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		//add reuqest header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("Accept-Language", "ca-es");
+        in.close();
 		
-		//Query string
-		String urlParameters = "categoryid=7";
-		
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
+	//print result
+        System.out.println(response.toString());
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-		
-		//print result
-		System.out.println(response.toString());
-
-	}
-
+    }
 }
